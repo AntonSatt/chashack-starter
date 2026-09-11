58341824ae40ba6a1f8af40f51d4331c3c773e75
Alex <letmebetomorrow@gmail.com>
2026-09-11 14:09:16 +0200

    weather is ready

--- step_8.js
+++ step_8.js
@@ -1,8 +1,6 @@
-console.log('script is here')
-
 btnSteg8 = document.querySelector('.step8');
 headerStep1 = document.querySelector('.step1');
-console.log(btnSteg8, headerStep1);
+
 
 if (btnSteg8) {
   btnSteg8.addEventListener('click', changeHEaderColor)
@@ -17,7 +15,7 @@ function changeHEaderColor(){
 
   if (!btnSteg8) return;
   styles = getComputedStyle(headerStep1)
-  console.log(styles.color)
+
   headerStep1.style.color = styles.color == colors.red
     ? colors.yellow
     : colors.red;
