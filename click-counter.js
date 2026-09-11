94ba3a93716cd93974a69f7fadcc48928923d698
rickard.wredberg@gmail.com <rickard.wredberg@gmail.com>
2026-09-11 13:14:51 +0200

    klickräknare

--- click-counter.js
+++ click-counter.js
@@ -1,3 +1,5 @@
-const button = document.querySelector("button")
-const counter = document.getElementById("counter")
-button.addEventListener("click", () => counter.innerHTML++)
+var button = document.getElementById("click-counter"), count = 0;
+button.onclick = function(){
+    count += 1;
+    button.innerHTML = "Klicka mig!: " + count;
+};
