4b8e4710ae774b1c3fcd1d773f8e148cc6ced6be
Recall12345 <Noor-sror@hotmail.com>
2026-09-11 14:20:18 +0200

    försök 1

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const nameButtons = document.querySelectorAll(".name-button");
+const personDescription = document.querySelector("#person-description");
 
 nameButtons.forEach((nameButton) => {
 	nameButton.addEventListener("click", () => {
-		alert(`Du klickade på ${nameButton.dataset.name}!`);
+		personDescription.textContent = `${nameButton.dataset.name}: ${nameButton.dataset.description}`;
 	});
 });
 
